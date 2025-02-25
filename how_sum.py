79ec10ae0ee219e4e83054d43e01b259569ecf37
= <=>
2025-02-25 16:36:14 +0300

    recursively get numbers in a list that can achieve a target_sum

--- how_sum.py
+++ how_sum.py
@@ -1,16 +1,21 @@
 from typing import List
-def how_sum(target_sum: int, numbers: int, current = []) -> List[int]:
+def how_sum(target_sum: int, numbers: int) -> List[int]:
     if target_sum == 0:
         return []
     if target_sum < 0:
         return None
     for number in numbers:
-        pass
+        remainder = target_sum - number
+        result = how_sum(remainder, numbers)
+        if result != None:
+            return result + [number]
+        how_sum(remainder ,numbers)
 
 def main() -> None:
-    numbers: List[int] = [5, 3, 4, 7]
-    target_sum: int = 7
-    print(f"Numbers that can achieve target sum of {target_sum} are: {how_sum(target_sum, numbers)}")
+    numbers: List[int] = [3, 7]
+    target_sum: int = 11
+    result = how_sum(target_sum, numbers)
+    print(f"Numbers in {numbers} that can achieve target sum of {target_sum} are: {result}")
 
 if __name__ == "__main__":
     main()
